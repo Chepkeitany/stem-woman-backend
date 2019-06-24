74a36ea6840831edabe652ae2bdb9ad9a1c39e95
Anne Chepkeitany <rerimoianne.4@gmail.com>
2019-06-25 00:36:44 +0300

    Add deployment files

--- application/config/env/production.js
+++ application/config/env/production.js
@@ -250,10 +250,9 @@ module.exports = {
     * > Be sure to use the right protocol!  ("http://" vs. "https://")         *
     *                                                                          *
     ***************************************************************************/
-    // onlyAllowOrigins: [
-    //   'https://example.com',
-    //   'https://staging.example.com',
-    // ],
+    onlyAllowOrigins: [
+      'https://api.stem-woman.info',
+    ],
 
 
     /***************************************************************************
@@ -370,26 +369,6 @@ module.exports = {
   * > See config/custom.js for more info on how to configure these options. *
   *                                                                         *
   ***************************************************************************/
-  custom: {
-    baseUrl: 'https://example.com',
-    internalEmailAddress: 'support@example.com',
-
-    // mailgunDomain: 'mg.example.com',
-    // mailgunSecret: 'key-prod_fake_bd32301385130a0bafe030c',
-    // stripeSecret: 'sk_prod__fake_Nfgh82401348jaDa3lkZ0d9Hm',
-    //--------------------------------------------------------------------------
-    // /\   OR, to avoid checking them in to version control, you might opt to
-    // ||   set sensitive credentials like these using environment variables.
-    //
-    // For example:
-    // ```
-    // sails_custom__mailgunDomain=mg.example.com
-    // sails_custom__mailgunSecret=key-prod_fake_bd32301385130a0bafe030c
-    // sails_custom__stripeSecret=sk_prod__fake_Nfgh82401348jaDa3lkZ0d9Hm
-    // ```
-    //--------------------------------------------------------------------------
-
-  },
 
 
 
---
+++ deployment/deploy.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+deploy_k8s() {
+	read -p "Deploying to k8s $environment environment in the $namespace namespace. Continue? (Y/n): " -n 1 -r
+	echo  #
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+		echo "Configuring configMaps..."
+    kubectl delete configmap $app-configmaps -n $namespace --cluster gke_kubernetes-intro-226510_europe-west1-b_stem-woman || echo "Failed to delete deployment configmap. OK for first time deployment."
+    kubectl create configmap $app-configmaps -n $namespace --from-env-file=configmaps/.env.$environment --cluster gke_kubernetes-intro-226510_europe-west1-b_stem-woman
+		echo "Configuring secrets..."
+    kubectl delete secret $app-secrets -n $namespace --cluster gke_kubernetes-intro-226510_europe-west1-b_stem-woman || echo "Failed to delete deployment secret. OK for first time deployment."
+    kubectl create secret generic $app-secrets  -n $namespace --from-env-file=secrets/.env.$environment  --cluster gke_kubernetes-intro-226510_europe-west1-b_stem-woman
+		echo "Configuring services..."
+		kubectl apply -n $namespace -f services --cluster gke_kubernetes-intro-226510_europe-west1-b_stem-woman
+		echo "Configuring ingress..."
+		kubectl apply -f ingress/$environment.yaml --cluster gke_kubernetes-intro-226510_europe-west1-b_stem-woman
+	fi
+
+  if [ $environment == "development" ]; then
+		echo "Configuring deployments..."
+		kubectl apply -n $namespace -f deployments/dev/deployment.yaml --cluster gke_kubernetes-intro-226510_europe-west1-b_stem-woman
+  else
+		echo "Configuring deployments..."
+		kubectl apply -n $namespace -f deployments/deployment.yaml --cluster gke_kubernetes-intro-226510_europe-west1-b_stem-woman
+  fi
+}
+
+app=stem-woman
+case $1 in
+    prod)
+        environment=production
+				namespace=$environment
+        deploy_k8s
+        ;;
+    *)
+        echo "
+usage: deploy [prod]
+"
+		;;
+esac
